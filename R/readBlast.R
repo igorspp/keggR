3e5178b8f729d451a2a327ca661f8858dc94a16a
Igor Pessi <38423985+igorspp@users.noreply.github.com>
2020-03-31 14:44:29 +0300

    Updated

--- R/readBlast.R
+++ R/readBlast.R
@@ -10,18 +10,30 @@
 
 # ADD CHECK TO NOT ALLOW COMMAS IN SEQUENCE NAME
 
-readBlast <- function(file) {
-  object <-  read_delim(file,
-                        delim = "\t",
-                        col_names = c("qseqid", "sseqid", "pident", "length", "mismatch", "gapopen", "qstart", "qend", "sstart", "send", "evalue", "bitscore"),
-                        col_types = cols_only(qseqid = col_character(), sseqid = col_character())) %>%
-    rename(sequence = qseqid, target = sseqid) %>%
+readBlast <- function(file, e_value = FALSE) {
+  data <-  read_delim(file,
+                      delim = "\t",
+                      col_names = c("qseqid", "sseqid", "pident", "length", "mismatch", "gapopen", "qstart", "qend", "sstart", "send", "evalue", "bitscore"),
+                      col_types = cols_only(qseqid = col_character(), sseqid = col_character(), evalue = col_double())) %>%
+    rename(sequence = qseqid, target = sseqid, e_value = evalue) %>%
+    arrange(sequence, target)
+
+  e_values <- data %>%
+    pull(e_value)
+
+  data <- data %>%
+    select(-e_value) %>%
     group_by(target) %>%
     mutate(sequence = paste0(sequence, collapse = ",")) %>%
     unique %>%
     ungroup
 
-  results <- new("blast_tbl", data = object)
+  if (isTRUE(e_value)) {
+    results <- new("blast_tbl", data = data, e_value = e_values)
+  }
+  else {
+    results <- new("blast_tbl", data = data)
+  }
 
   return(results)
 }
