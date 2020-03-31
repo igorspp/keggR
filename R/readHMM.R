d4d22a5d988c554ea27d3f58cc66121657bf5029
Igor Pessi <38423985+igorspp@users.noreply.github.com>
2020-03-31 14:46:20 +0300

    Updated

--- R/readHMM.R
+++ R/readHMM.R
@@ -10,18 +10,30 @@
 
 # ADD CHECK TO NOT ALLOW COMMAS IN SEQUENCE NAME
 
-readHMM <- function(file) {
-  object <-  read_delim(file,
-                        delim = "\t",
-                        col_names = c("sequence", "KO", "evalue", "score"),
-                        col_types = cols_only(sequence = col_character(), KO = col_character())) %>%
-    group_by(KO) %>%
+readHMM <- function(file, e_value = FALSE) {
+  data <-  read_delim(file,
+                      delim = "\t",
+                      col_names = c("sequence", "target", "e_value", "score"),
+                      col_types = cols_only(sequence = col_character(), target = col_character(), e_value = col_double())) %>%
+    arrange(sequence, target)
+
+  e_values <- data %>%
+    pull(e_value)
+
+  data <- data %>%
+    select(-e_value) %>%
+    group_by(target) %>%
     mutate(sequence = paste0(sequence, collapse = ",")) %>%
     unique %>%
     ungroup
-  
-  results <- new("blast_tbl", data = object)
-  
+
+  if (isTRUE(e_value)) {
+    results <- new("blast_tbl", data = data, e_value = e_values)
+  }
+  else {
+    results <- new("blast_tbl", data = data)
+  }
+
   return(results)
 }
 
